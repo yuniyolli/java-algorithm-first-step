681ecc2e6f0c59a88b41fad2c8ea902480c0486a
yuniyolli <yuniyolli@gmail.com>
2023-07-15 22:30:05 +0900

    add: FromIndexN

---
+++ src/main/java/lv0/array/FromIndexN.java
@@ -0,0 +1,29 @@
+package lv0.array;
+
+import java.util.Arrays;
+
+public class FromIndexN {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[num_list.length - (n - 1)];
+        //int[] answer = new int[3];
+        int count = 0;
+
+        for (int i = 0; i < num_list.length ; i++) {
+
+            if (i >= n - 1) {
+                //int count = 0; //-> 했을때는 첫번째 자리에 계속 더해졌는데 왜그렇지? -> 매번 다시 0으로 초기화 되기 때문!!!
+                answer[count] += num_list[i];
+                count++;
+                //System.out.println(count);
+            }
+
+        }
+        //System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new FromIndexN().solution(new int[]{2, 1, 6}, 3);
+        new FromIndexN().solution(new int[]{5, 2, 1, 7, 5}, 2);
+    }
+}
