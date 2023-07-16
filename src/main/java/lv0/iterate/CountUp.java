6026bc2c9d89e922282d314041ccfa38beebd45e
yuniyolli <yuniyolli@gmail.com>
2023-07-16 17:04:09 +0900

    add: SumBiggerThanN, CountUp, CalculationByArrLen

---
+++ src/main/java/lv0/iterate/CountUp.java
@@ -0,0 +1,13 @@
+package lv0.iterate;
+
+public class CountUp {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[(end + 1) - start] ;
+        int count = 0;
+        for (int i = start; i < end + 1; i++) {
+            answer[count] += i;
+            count++;
+        }
+        return answer;
+    }
+}
---
+++ src/main/java/lv0/method/CalculationByArrLen.java
@@ -0,0 +1,42 @@
+package lv0.method;
+
+import java.util.Arrays;
+
+public class CalculationByArrLen {
+    /*
+
+     */
+    public int[] solution(int[] arr, int n) {
+        int[] answer = new int[arr.length];
+        int cnt = 0;
+        // arr의 길이가 홀수라면
+        if (arr.length % 2 != 0 ) {
+            // arr짝수 인덱스 + n
+            for (int i = 0; i < arr.length ; i++) {
+                if (i % 2 == 0) {
+                    answer[cnt] = arr[i] + n;
+                } else answer[cnt] = arr[i];
+                cnt++;
+            }
+            // arr의 길이가 짝수라면
+        } else {
+            for (int i = 0; i < arr.length ; i++) {
+                // arr홀수 인덱스 + n
+                if (i % 2 != 0) {
+                    answer[cnt] = arr[i] + n;
+                } else answer[cnt] = arr[i];
+
+                cnt++;
+            }
+        }
+        //System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CalculationByArrLen calculationByArrLen = new CalculationByArrLen();
+        calculationByArrLen.solution(new int[]{49, 12, 100, 276, 33}, 27);
+        calculationByArrLen.solution(new int[]{444, 555, 666, 777}, 100);
+
+    }
+}
