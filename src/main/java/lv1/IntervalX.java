4afb76b5780b58d8036859682c603a3ba0ece43b
yuniyolli <yuniyolli@gmail.com>
2023-07-30 11:07:42 +0900

    add: programmers lv1 -AverageOfArray -InvervalX -SumOfDivisor

---
+++ src/main/java/lv1/IntervalX.java
@@ -0,0 +1,22 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class IntervalX {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            // answer[i] = (x + (x * i));
+            answer[i] = answer[i - 1] + x;
+        }
+        //System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        new IntervalX().solution(10000000, 1000);
+    }
+}
