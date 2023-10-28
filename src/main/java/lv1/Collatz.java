173ca342ddd9ebb136d54b3c7ec4f1a9413aa5f1
yuniyolli <yuniyolli@gmail.com>
2023-10-28 15:43:28 +0900

    add Collats, FindKim

---
+++ src/main/java/lv1/Collatz.java
@@ -0,0 +1,52 @@
+package lv1;
+/*
+1937년 Collatz란 사람에 의해 제기된 이 추측은,
+주어진 수가 1이 될 때까지 다음 작업을 반복하면, 모든 수를 1로 만들 수 있다는 추측입니다.
+작업은 다음과 같습니다.
+
+1-1. 입력된 수가 짝수라면 2로 나눕니다.
+1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+ */
+public class Collatz {
+    public int solution(long num) {
+        // answer == 반복횟수
+        int answer = 0;
+
+        // 결과 값이 1이 될 때까지 반복한다 (while num == 1)
+        // num 이 1보다 큰 동안만 반복한다
+        while (num > 1) {
+            // while (num != 1)
+
+            // 입력된 수가 짝수라면 2로 나눈다. (num % 2 == 0 ? num / 2 )
+            // 입력된 수가 홀수라면 3을 곱하고 1을 더한다 ( : (num * 3) + 1 )
+            num = (num % 2 == 0) ? num / 2 : (num * 3) + 1 ;
+            // 1회 반복 시 answer값 추가
+            answer++;
+
+            // 500 반복할 때까지 1이 되지 않으면 -1 반환
+
+            if (answer >= 500) {
+                answer = -1;
+                break;
+            }
+
+
+        }
+
+        System.out.println("answer : " + answer);
+        System.out.println("num :" + num);
+
+
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        new Collatz().solution(626331);
+//        new Collatz().solution(16);
+//        new Collatz().solution(6);
+    }
+}
---
+++ src/main/java/lv1/FindKim.java
@@ -0,0 +1,39 @@
+package lv1;
+
+import java.util.List;
+
+//String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+public class FindKim {
+    public String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = String.format("김서방은 %d에 있다", i);
+                // 효율성을 위한 break추가
+                break;
+            }
+        }
+        // for문 대체(하지만 for문이 더 빠름):
+        // int x = Arrays.asList(seoul).indexOf("Kim");
+
+        // 속도 올리는 법: StringBuilder
+        // return new StringBuilder("김서방은 ").append(x).append("에 있다");
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindKim().solution(new String[] {"Jane", "Kim"}));
+
+        // 실행문 예시
+        /*
+        FindKim kim = new FindKim();
+        String[] names = {"Queen", "Tod", "Kim"};
+        System.out.println(kim.findKim(names));
+         */
+    }
+}
+
+
+
