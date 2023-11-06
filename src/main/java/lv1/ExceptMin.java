6dba306f55a94209d295066775bddc477bce5d70
yuniyolli <yuniyolli@gmail.com>
2023-11-06 17:58:35 +0900

    add AddNu s, ExceptMin

---
+++ src/main/java/lv1/ExceptMin.java
@@ -0,0 +1,33 @@
+package lv1;
+
+import java.util.Arrays;
+
+public class ExceptMin {
+    public int[] solution(int[] arr) {
+        // 길이로 -1리턴
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            return answer;
+        } else {
+            // 가장 작은 값 찾기
+            int[] answer = new int[arr.length - 1];
+            int minArr = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if(arr[i] < arr[minArr]) {
+                    minArr = i;
+            }
+            }
+            int minVal = arr[minArr];
+            int idx = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                if(arr[i] > minVal) {
+                    answer[idx] += arr[i];
+                    idx++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
